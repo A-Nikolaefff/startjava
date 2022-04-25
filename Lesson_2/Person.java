7f44fd9ce748068efa17efd90f2ba2bb6274fab8
A-Nikolaefff <nikron-d@mail.ru>
2022-04-25 16:33:46 +0300

    File Person.java has been added

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    private boolean sex = true;
+    private String name = "Jack";
+    private int weight = 90;
+    private int height = 185;
+    private int age = 45;
+
+    public void walk() {
+        System.out.println(name + " is walking.");
+    }
+
+    public void run() {
+        System.out.println(name + " is running.");
+    }
+
+    public void sit() {
+        System.out.println(name + " is siting.");
+    }
+
+    public void speak() {
+        System.out.println(name + " is speaking.");
+    }
+
+    public void learnJava() {
+        System.out.println(name + " is learning Java.");
+    }
+}
