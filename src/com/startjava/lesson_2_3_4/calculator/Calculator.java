580d800a64531d4a56be7544b35cccea44d64a98
A-Nikolaefff <nikron-d@mail.ru>
2022-05-11 13:42:22 +0300

    Update Calculator - fix the mentor's remarks dated 05/11/2022

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,13 +2,10 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
     public static int calculate(String expression) throws ArithmeticException {
-        double number1;
-        double number2;
-        char sign;
         String[] partsExpression = expression.split(" ");
-        number1 = Double.parseDouble(partsExpression[0]);
-        number2 = Double.parseDouble(partsExpression[2]);
-        sign = partsExpression[1].charAt(0);
+        double number1 = Double.parseDouble(partsExpression[0]);
+        double number2 = Double.parseDouble(partsExpression[2]);
+        char sign = partsExpression[1].charAt(0);
         if ((number1 <= 0 || number1 % 1 != 0) || (number2 <= 0 || number2 % 1 != 0)) {
             throw new ArithmeticException("Ошибка! Числа должны быть целые положительные!");
         }
